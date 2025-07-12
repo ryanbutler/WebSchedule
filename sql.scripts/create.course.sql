c97bdec1f659137d5207206fc6b56466a1f15689
Ryan Butler <ryan.c.butler@hotmail.com>
2025-07-12 10:16:36 -0500

    updated table script creation for course

--- sql.scripts/create.course.sql
+++ sql.scripts/create.course.sql
@@ -1,7 +1,7 @@
 USE [WebSchedule]
 GO
 
-/****** Object:  Table [dbo].[Course]    Script Date: 7/1/2025 6:20:29 PM ******/
+/****** Object:  Table [dbo].[Course]    Script Date: 7/12/2025 10:15:57 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,8 +12,8 @@ CREATE TABLE [dbo].[Course](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Title] [varchar](50) NULL,
 	[TotalSeats] [varchar](50) NULL,
-	[RemainingSeats] [varchar](50) NULL,
-	[RoomId] [int] NULL,
+	[RemainingSeats] [int] NULL,
+	[RoomId] [int] NOT NULL,
 	[StartTime] [varchar](50) NULL,
 	[EndTime] [varchar](50) NULL,
 	[InstructorId] [int] NOT NULL,
@@ -25,5 +25,40 @@ CREATE TABLE [dbo].[Course](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_Instructor] FOREIGN KEY([InstructorId])
+REFERENCES [dbo].[Instructor] ([Id])
+GO
+
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_Instructor]
+GO
+
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_Location] FOREIGN KEY([LocationId])
+REFERENCES [dbo].[Location] ([Id])
+GO
+
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_Location]
+GO
+
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_MeetDay] FOREIGN KEY([MeetDayId])
+REFERENCES [dbo].[MeetDay] ([Id])
+GO
+
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_MeetDay]
+GO
+
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_MeetType] FOREIGN KEY([MeetTypeId])
+REFERENCES [dbo].[MeetType] ([Id])
+GO
+
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_MeetType]
+GO
+
+ALTER TABLE [dbo].[Course]  WITH CHECK ADD  CONSTRAINT [FK_Course_Room] FOREIGN KEY([RoomId])
+REFERENCES [dbo].[Room] ([Id])
+GO
+
+ALTER TABLE [dbo].[Course] CHECK CONSTRAINT [FK_Course_Room]
 GO
 
